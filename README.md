12c24213b24f3ef4c161cb03fdebb7e3747c2a6f
tosin-dawodu <76872739+tosin-dawodu@users.noreply.github.com>
2022-08-15 23:34:39 +0100

    corrected responsiveness, UI and UX

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Simple biography page with minor implementation of BootStrap as a framework and some JS 
+Simple biography website with minor implementation of BootStrap as a framework and vanilla JS.
+
+Background music credit: Commander Ebenezer Obey-Eni ri nkan re
--- js/landing.js
+++ js/landing.js
@@ -1,5 +1,7 @@
+alert("Double-tap on your screen to play or pause background music.");
+
 const audioPlay = document.getElementById("audio");
 function togglePlay() {
   return audioPlay.paused ? audioPlay.play() : audioPlay.pause();
 }
-document.addEventListener("click", togglePlay);
+document.addEventListener("dblclick", togglePlay);
